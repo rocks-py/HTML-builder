ff84591a8564d57a0afdcab0f42e75f081de885e
Artem <75018018+rocks-py@users.noreply.github.com>
2022-05-20 06:53:20 -0700

    feat: solve task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 
+
+let sourceDir = path.join(__dirname, 'files');
+let destDir = path.join(__dirname, 'files-copy');
+
+function callback(err) {
+  if (err) throw err;
+}
+
+fs.rm(destDir, {recursive: true}, () => {
+  fs.mkdir(destDir, {recursive: true}, () => {
+    fs.promises.readdir(sourceDir, {withFileTypes: true}).then(filenames => {
+      for (const file of filenames) {
+        if (file.isFile()) {
+          let sourceFile = path.join(sourceDir, file.name);
+          let destFile = path.join(destDir, file.name);
+          fs.copyFile(sourceFile, destFile, callback);
+        }
+      }
+    });
+  });
+});
